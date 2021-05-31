36a0e8b896b16ca01abe70f024073cbe49d37bc0
OderoOluoch <oderoluoch@gmail.com>
2021-05-31 11:53:41 +0300

    Fist tesm method

--- src/main/java/ScrabbleScorer.java
+++ src/main/java/ScrabbleScorer.java
@@ -15,7 +15,7 @@ public class ScrabbleScorer {
 
         //initialize the array of individual letter scores.
         int[] scores = new int[]{
-                1,3,2,1,4,2,4,1,8,5,1,3,
+                1,3,3,2,1,4,2,4,1,8,5,1,3,
                 1,1,3,10,1,1,1,1,4,4,8,4,10
         };
 
--- src/test/java/ScrabbleScorerTest.java
+++ src/test/java/ScrabbleScorerTest.java
@@ -1,5 +1,14 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScrabbleScorerTest {
+    private final ScrabbleScorer scorer = new ScrabbleScorer();
 
+    @Test
+    public void testTileScore(){
+        assertEquals(1,scorer.scoreForTile('A'));
+        assertEquals(3,scorer.scoreForTile('B'));
+        assertEquals(10,scorer.scoreForTile('Q'));
+    }
 }
